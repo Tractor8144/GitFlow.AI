6885c08948f6637bdbf0ca4decb4a7117a8f771f
Detergent4316 <maharshi@midasit.in>
2025-04-29 22:53:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from gitflow_ai.git_command_handler import GitHandler
 from gitflow_ai.git_router import GitCommandRouter
 from gitflow_ai.test_nlp_parser import TestNLPParser
 from gitflow_ai.input_manager import InputManager
+from gitflow_ai.openai_client import OpenAIClient
+from gitflow_ai.nlp_parser import NLPParser
 import sys
 
 
@@ -13,7 +15,9 @@ def main():
     repo = Repo("D:\\Projects\\FastAPI_Basic\\FastAPI-Basic")
     git_handler = GitHandler(repo)
     router = GitCommandRouter(git_handler)
-    parser = TestNLPParser()
+    # parser = TestNLPParser()
+    openai = OpenAIClient()
+    parser = NLPParser(openai)
 
     while True:
         user_input = input_manager.get_input()
@@ -21,8 +25,11 @@ def main():
             print("Quitting GitFlow.AI. Goodbye!")
             sys.exit(0)
         print(f'You asked command {user_input}')
-
-        action_list = parser.parse(user_input)
+        try:
+            action_list = parser.parse(user_input)
+        except:
+            print("Sorry! Could not understand the message. Please try again!")
+            continue
         router.route_list(action_list)
 
 
