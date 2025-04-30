15119f5ad1476e09fe99a0fc13a2a48338b0a46e
Maharshi <maharshi@midasit.com>
2025-04-30 12:48:30 +0530

    Go via ActionDispatcher instead of GitCommandRouter for added repo management functionalities

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from gitflow_ai.test_nlp_parser import TestNLPParser
 from gitflow_ai.input_manager import InputManager
 from gitflow_ai.openai_client import OpenAIClient
 from gitflow_ai.nlp_parser import NLPParser
+from gitflow_ai.action_dispatcher import ActionDispatcher
+from gitflow_ai.repo_manager import RepoManager
 import sys
 
 
@@ -12,12 +14,13 @@ def main():
     print("Welcome to GitFlow.AI")
 
     input_manager = InputManager()
-    repo = Repo("D:\\Projects\\FastAPI_Basic\\FastAPI-Basic")
-    git_handler = GitHandler(repo)
-    router = GitCommandRouter(git_handler)
-    # parser = TestNLPParser()
+    # repo = Repo("D:\\Projects\\FastAPI_Basic\\FastAPI-Basic")
+    # git_handler = GitHandler(repo)
+    # router = GitCommandRouter(git_handler)
     openai = OpenAIClient()
     parser = NLPParser(openai)
+    repo_manager = RepoManager()
+    action_dispatcher = ActionDispatcher(repo_manager)
 
     while True:
         user_input = input_manager.get_input()
@@ -30,7 +33,8 @@ def main():
         except:
             print("Sorry! Could not understand the message. Please try again!")
             continue
-        router.route_list(action_list)
+        action_dispatcher.dispatch(action_list)
+        # router.route_list(action_list)
 
 
 if __name__ == "__main__":
